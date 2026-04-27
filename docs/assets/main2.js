35bf14bbd59f596df404cffb1f73a9811e77e0ff
ELCRND <dmtchenko@gmail.com>
2026-04-27 19:33:20 +0300

    modified:   docs/assets/main.css 	modified:   docs/assets/main.js 	modified:   docs/assets/main.js.map 	new file:   docs/assets/main2.css 	new file:   docs/assets/main2.js 	new file:   docs/assets/main2.js.map 	modified:   docs/index.html 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js 	modified:   src/scss/blocks/_header.scss 	modified:   src/scss/blocks/_reviews.scss 	modified:   src/scss/main.scss

---
+++ docs/assets/main2.js
@@ -0,0 +1,2 @@
+const e=""+new URL("main.css",import.meta.url).href;export{e as default};
+//# sourceMappingURL=main2.js.map
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import "./scss/main.scss";
 
 import { Header } from "./js/Header";
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+window.addEventListener('load',async()=>{
+
+try {
+    const { default: cssUrl } = await import('./scss/main.scss?url')
+    
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = cssUrl
+    document.head.appendChild(link)
+
+    console.log('Поздний SCSS загружен:', cssUrl)
+  } catch (err) {
+    console.error('Ошибка загрузки позднего CSS:', err)
+  }
+
+
 
 new Header();
 
 const swiper = new Swiper(".reviews__slider", {
+  modules: [Navigation],
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
@@ -27,3 +49,4 @@ const swiper = new Swiper(".reviews__slider", {
     },
   },
 });
+})
